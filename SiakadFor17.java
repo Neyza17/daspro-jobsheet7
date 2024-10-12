c487b3ce66aa866d586d881c73339afbef6ea632
Neyza17 <nesyaratuanastasya@gmail.com>
2024-10-12 11:42:54 +0700

    second commit
    second commit

--- SiakadFor17.java
+++ SiakadFor17.java
@@ -4,7 +4,9 @@ public class SiakadFor17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        int jumlahMahasiswa = 10;
+        double batasLulus = 60, nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i +": ");
@@ -14,10 +16,15 @@ public class SiakadFor17 {
             }
             if(nilai < terendah) {
                 terendah = nilai;
+            }else {
+                tidakLulus++;
             }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
+
 
     }
 }
